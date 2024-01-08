e1943ca3119a4fb79f51d0678ee7d6606b963d21
rjmnhr1 <renjith.cm@refactor.academy>
2024-01-09 01:25:31 +0530

    update

--- src/components/axios/index.js
+++ src/components/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "https://awakenbackend-e14a4ayo.b4a.run", // Replace with your API base URL
+  baseURL: "https://awakenbackendnew-sjn7gyfg.b4a.run", // Replace with your API base URL
   // baseURL: "http://localhost:8003",
 });
 
